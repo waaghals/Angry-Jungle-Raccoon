fa437b397795670a933f3ef127e3bfcfc47f7a5f
RobinCollard <name24@hotmail.nl>
2013-12-20 11:58:54 +0100

    Models update

--- Project views/BarometerStudent/BarometerStudent/App_Start/AuthConfig.cs
+++ Project views/BarometerStudent/BarometerStudent/App_Start/AuthConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Web.WebPages.OAuth;
-using BarometerStudent.Models;
+using BarometerStudent;
 
 namespace BarometerStudent
 {
