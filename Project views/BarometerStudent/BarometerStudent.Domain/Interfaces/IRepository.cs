dca2634c7790afb40305b5a45d69a8bad8f73dda
Yorick666 <yorickvanklinken@gmail.com>
2013-12-20 12:39:48 +0100

    Start van repositories

---
+++ Project views/BarometerStudent/BarometerStudent.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarometerStudent.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+
+        Boolean Delete(T entity);
+        Boolean Delete(int id);
+        object Get(int id);
+        IEnumerable<T> GetAll();
+        void Insert(T entity);
+        void Save();
+        Boolean Update(T entity);
+
+    }
+}
---
+++ Project views/BarometerStudent/BarometerStudent.Domain/Repositories/GenericRepository.cs
@@ -0,0 +1,83 @@
+﻿using BarometerStudent.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarometerStudent.Domain.Repositories
+{
+    abstract class GenericRepository<T> : IRepository<T>
+    {
+
+        public bool Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IRepository<T>.Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IRepository<T>.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        object IRepository<T>.Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IRepository<T>.Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
