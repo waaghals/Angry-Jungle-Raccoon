fefda5606dd28e72b653088487c520dcc27fca6b
Edwin Hattink <edwinhattink@me.com>
2013-12-22 11:36:40 +0100

    Model verplaatst naar database project, zelfde solution.

---
+++ Project views/BarometerStudent/BarometerDomain/Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarometerDomain
+{
+    class Context  :DbContext
+    {
+        public Context()
+            : base("AngryJungleRaccoon")
+        {
+
+        }
+        public DbSet<Evaluation> Evaluations { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectPeriod> ProjectPeriods { get; set; }
+        public DbSet<ScoreType> ScoreTypes { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
