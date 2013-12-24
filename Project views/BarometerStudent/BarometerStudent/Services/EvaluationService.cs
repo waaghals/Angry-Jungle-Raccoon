f763ac4a64fe000d9a4065880329ed060eb2fb41
Patrick Berenschot <patrick.stroef@gmail.com>
2013-12-24 15:57:32 +0100

    Added Services base to project, included BarometerDomain in references

--- Project views/BarometerStudent/BarometerStudent/Services/EvaluationService.cs
+++ Project views/BarometerStudent/BarometerStudent/Services/EvaluationService.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BarometerDomain.Repositories;
 
 public class EvaluationService : ICrudService<Evaluation>, IEvaluationService
 {
