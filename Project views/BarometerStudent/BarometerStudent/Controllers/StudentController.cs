1f6e20fcb39b0ecf6ceea24cb8299630a378258b
RobinCollard <name24@hotmail.nl>
2014-01-15 17:15:15 +0100

    Sort toegevoegd

--- Project views/BarometerStudent/BarometerStudent/Controllers/StudentController.cs
+++ Project views/BarometerStudent/BarometerStudent/Controllers/StudentController.cs
@@ -19,18 +19,32 @@ public class StudentController : Controller
         IList<string> projectNames = new List<string>();
         List<Evaluation> evaluationList = new List<Evaluation>();
         Dictionary<string, List<Evaluation>> projectsAvgEvaluations = new Dictionary<string, List<Evaluation>>();
-        foreach(Group group in student.Groups)
+        foreach (Group group in student.Groups)
         {
-            foreach(Project project in group.Project)
+            foreach (Project project in group.Project)
             {
                 projectNames.Add(project.Name);
-                evaluationList = (List<Evaluation>) project.GetAverageEvaluations(student);
+                evaluationList = (List<Evaluation>)project.GetAverageEvaluations(student);
+                evaluationList = BubbleSort(evaluationList);
                 projectsAvgEvaluations.Add(project.Name, evaluationList);
             }
         }
+
         ViewBag.projectNames = projectNames;
-        
         return View(projectsAvgEvaluations);
+    }
+
+    private List<Evaluation> BubbleSort(List<Evaluation> evaluationList)
+    {
+        for (int outer = evaluationList.Count - 1; outer >= 1; outer--)
+            for (int inner = 0; inner < outer; inner++) // inner loop (forward)
+                if ((evaluationList[inner].CompareTo(evaluationList[inner + 1]) == -1))
+                {
+                    Evaluation temp = evaluationList[inner];
+                    evaluationList[inner] = evaluationList[inner - 1];
+                    evaluationList[inner - 1] = temp;
+                }
+        return evaluationList;
     }
 }
 
