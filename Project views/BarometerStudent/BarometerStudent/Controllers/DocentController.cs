acd3ed89f240351b6410e49a6e26d400c1446e54
RobinCollard <name24@hotmail.nl>
2014-01-18 15:13:45 +0100

    Tutor cijfers inzien tot aan de tabjes.

--- Project views/BarometerStudent/BarometerStudent/Controllers/DocentController.cs
+++ Project views/BarometerStudent/BarometerStudent/Controllers/DocentController.cs
@@ -25,35 +25,44 @@ namespace BarometerStudent.Controllers
             UserRepository userrep = new UserRepository(new Context());
             User user = userrep.Get(userID);
             List<Student> studenten = user.MentorStudent.ToList<Student>();
+
             return View(studenten);
         }
 
-        [HttpGet]
         public ActionResult SelecteerProject()
         {
             ProjectRepository pr = new ProjectRepository(new Context());
-            SelectList sl = new SelectList(pr.ByTutor(/*tutorid*/userID), "Id", "Name");
-            return View(sl);
+
+            ViewBag.selectListProjects = new SelectList(pr.ByTutor(/*tutorid*/userID), "Id", "Name", "1");
+            return View();
         }
 
         [HttpPost]
-        public ActionResult SelecteerProject(Project project)
+        public ActionResult SelecteerProject(string project)
         {
-            if (ModelState.IsValid)
+            if (project != null)
             {
-                TempData["myProject"] = project;
+                int projectId = Convert.ToInt32(project);
+
+                ProjectRepository pr = new ProjectRepository(new Context());
+                Project myProject = pr.Get(projectId);
+
+                TempData["myProject"] = myProject;
+
                 return RedirectToAction("SelecteerTutorGroep", "Docent");
             }
             return View();
         }
 
-        [HttpGet]
         public ActionResult SelecteerTutorGroep()
         {
             if (TempData.ContainsKey("myProject"))
             {
+                TempData.Keep();
+
                 Project myProject = (Project)TempData["myProject"];
                 IList<Group> tutorGroupList = new List<Group>();
+
                 foreach(Group group in myProject.Groups)
                 {
                     if (group.Tutor.Id == userID)
@@ -62,7 +71,9 @@ namespace BarometerStudent.Controllers
                     }
                 }
                 SelectList sl = new SelectList(tutorGroupList, "Id", "Name");
-                return View(sl);
+
+                ViewBag.selectListGroups = sl;
+                return View();
             }
             else
             {
@@ -71,22 +82,47 @@ namespace BarometerStudent.Controllers
         }
 
         [HttpPost]
-        public ActionResult SelecteerTutorgroep(Group group)
+        public ActionResult SelecteerTutorgroep(string group)
         {
-            if(ModelState.IsValid)
+            if (group != null)
             {
-                TempData["group"] = group;
-                return RedirectToAction("ViewGroup", "Docent");
+                int groupId = Convert.ToInt32(group);
+
+                GroupRepository gr = new GroupRepository(new Context());
+                Group myGroup = gr.Get(groupId);
+
+                TempData["myGroup"] = myGroup;
+
+                return RedirectToAction("ViewGroep", "Docent");
             }
             return View();
         }
 
         public ActionResult ViewGroep()
         {
-            if(TempData.ContainsKey("group"))
+            if (TempData.ContainsKey("myGroup") && TempData.ContainsKey("myProject"))
             {
-                Group group = (Group)TempData["group"];
-                return View(group);
+                TempData.Keep();
+
+                Group group = (Group)TempData["myGroup"];
+                Project project = (Project)TempData["myProject"];
+
+                List<ProjectPeriod> projectPeriodList = new List<ProjectPeriod>();
+                
+                foreach(Project pr in group.Project)
+                {
+                    foreach(ProjectPeriod period in pr.ProjectPeriod)
+                    {
+                        if(pr.Id == project.Id)
+                        {
+                            projectPeriodList.Add(period);
+                        }
+                    }
+                }
+
+                ViewBag.project = project;
+                ViewBag.group = group;
+                return View(projectPeriodList);
             }
             else
             {
